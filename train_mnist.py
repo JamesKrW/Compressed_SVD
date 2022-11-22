2053d510e332efa71b63cacf26ef68fffb35ce2e
Ray Andrew <4437323+rayandrew@users.noreply.github.com>
2022-11-21 21:30:03 -0600

    add metrics timing

--- train_mnist.py
+++ train_mnist.py
@@ -2,8 +2,10 @@ import argparse
 from mlp import MLP
 import numpy as np
 from data_set import mnist
+import metrics
 
 
+@metrics.timing
 def train_mnist(args):
     epochs = args.epochs
     batch_size = args.batch_size
@@ -11,15 +13,14 @@ def train_mnist(args):
     X_train = train_set[0]
     Y_train = train_set[1]
 
-    model = MLP(args.model_shape,lr=args.learning_rate)
-
+    model = MLP(args.model_shape, lr=args.learning_rate)
 
     for epoch in range(epochs):
         indexs = np.arange(len(X_train))
         steps = len(X_train) // batch_size
         np.random.shuffle(indexs)
         for i in range(steps):
-            ind = indexs[i*batch_size:(i+1)*batch_size]
+            ind = indexs[i * batch_size : (i + 1) * batch_size]
             x = X_train[ind]
             y = Y_train[ind]
             loss = model(x, y)
@@ -28,16 +29,21 @@ def train_mnist(args):
 
             if (i + 1) % 100 == 0:
                 accuracy = model.validate(valid_set[0], valid_set[1])
-                print('Epoch[{}/{}] \t Step[{}/{}] \t Loss = {:.6f} \t Acc = {:.3f}'.format(epoch+1, epochs, i+1, steps, loss, accuracy))
+                print(
+                    "Epoch[{}/{}] \t Step[{}/{}] \t Loss = {:.6f} \t Acc = {:.3f}".format(
+                        epoch + 1, epochs, i + 1, steps, loss, accuracy
+                    )
+                )
         model.lr_step()
-    print('Test Accuracy = ', model.validate(test_set[0], test_set[1]))
+    print("Test Accuracy = ", model.validate(test_set[0], test_set[1]))
+
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--batch_size',default=32,type=int)           
-parser.add_argument('--epochs',default=20,type=int)      
-parser.add_argument('--data_path', default='./data/mnist.pkl.gz')
-parser.add_argument('--model_shape',default=[784,20,20,10],type=list)
-parser.add_argument('--learning_rate',default=0.01,type=float)   
+parser.add_argument("--batch_size", default=32, type=int)
+parser.add_argument("--epochs", default=20, type=int)
+parser.add_argument("--data_path", default="./data/mnist.pkl.gz")
+parser.add_argument("--model_shape", default=[784, 20, 20, 10], type=list)
+parser.add_argument("--learning_rate", default=0.01, type=float)
 args = parser.parse_args()
 
 
