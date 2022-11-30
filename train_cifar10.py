68619160887ee9c48614da23e5ea7c615ae9097f
Ray Andrew <4437323+rayandrew@users.noreply.github.com>
2022-11-30 13:14:17 -0600

    set default args in cifar10

--- train_cifar10.py
+++ train_cifar10.py
@@ -86,7 +86,7 @@ def main(args):
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--batch_size", default=32, type=int)
-parser.add_argument("--epochs", default=20, type=int)
+parser.add_argument("--epochs", default=60, type=int)
 parser.add_argument("--k", default=5, type=int)
 parser.add_argument(
     "--data_path", default="./data/cifar-10-binary.tar.gz"
@@ -95,7 +95,7 @@ parser.add_argument(
     "--model_shape", default=[3072, 20, 20, 10], type=list
 )
 parser.add_argument("--learning_rate", default=0.01, type=float)
-parser.add_argument("--l2_lambda", default=0.01, type=float)
+parser.add_argument("--l2_lambda", default=0.00, type=float)
 parser.add_argument(
     "--pruning",
     help="Enable pruning",
