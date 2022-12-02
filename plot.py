68372acf0b92d3e5b0536b1e72073965a59079d0
Ray Andrew <4437323+rayandrew@users.noreply.github.com>
2022-12-02 03:29:38 -0600

    add plot and final data

--- plot.py
+++ plot.py
@@ -8,14 +8,19 @@ import pandas as pd
 from matplotlib import ticker
 
 COLUMNS = ["test_acc", "test_time", "mem_size", "persisted_size"]
+GROUPED_COLUMNS = [
+    ["test_acc", "test_time"],
+    "mem_size",
+    "persisted_size",
+]
 COLUMN_HUMANIZE = [
     "Accuracy",
     "Time (in ms)",
     "Memory Size (in KB)",
-    "Persisted Size (in KB)",
+    "Persistence Size (in KB)",
 ]
 FIG_SIZE = (4, 3)
-# FILES = ["mnist.csv", "cifar.csv"]
+FIXED_LISTS = ["fixed_k", "fixed_sigma"]
 
 # each plot is one line
 MNIST_FILES = [
@@ -25,15 +30,15 @@ MNIST_FILES = [
     "mnist_non_pruned_single_layer.csv",
 ]
 CIFAR10_FILES = [
-    "cifar_non_pruned_double_layer.csv",
-    "cifar_pruned_double_layer.csv",
-    "cifar_pruned_single_layer.csv",
-    "cifar_non_pruned_single_layer.csv",
+    "cifar10_non_pruned_double_layer.csv",
+    "cifar10_pruned_double_layer.csv",
+    "cifar10_pruned_single_layer.csv",
+    "cifar10_non_pruned_single_layer.csv",
 ]
 
 ORIGINAL_COLOR = "green"
 ORIGINAL_MARKER = "o"
-ALL_COLORS = ["maroon", "orange", "purple", "black"]
+ALL_COLORS = ["maroon", "orange", "blue", "black"]
 ALL_MARKERS = ["v", "s", "D", "X"]
 
 # COLOR = cycle(ALL_COLORS[: len(MNIST_FILES) + 1])
@@ -45,19 +50,36 @@ def second_to_ms_formatter(x, pos):
     return f"{x * 1000:g} ms"
 
 
-def plot_column_from_dfs(ax: plt.Axes, dfs: List[pd.DataFrame], y: str, x: str = "k"):
+def plot_column_from_dfs(
+    ax: plt.Axes,
+    dfs: List[pd.DataFrame],
+    original: pd.DataFrame,
+    y: str,
+    x: str = "k",
+    fixed: str = "k",
+):
+    if y in ["mem_size", "persisted_size"] and "k" not in fixed:
+        ax.axhline(
+            original[y][0],
+            c=ORIGINAL_COLOR,
+            linestyle="--",
+            label="Original",
+            markersize=3,
+            marker=ORIGINAL_MARKER,
+        )
+
     for i, df in enumerate(dfs):
-        print(df.loc[df["original"] == 1])
-        # df[df["original"] == 1].plot(
-        #     x=x,
-        #     y=f"bc_{y}",
-        #     ax=ax,
-        #     c=ORIGINAL_COLOR,
-        #     linestyle="--",
-        #     label="Original",
-        #     markersize=3,
-        #     marker=ORIGINAL_MARKER,
-        # )
+        if y in ["test_acc", "test_time"] and i == 0:
+            df.plot(
+                x=x,
+                y=f"bc_{y}",
+                ax=ax,
+                c=ORIGINAL_COLOR,
+                linestyle="--",
+                label="Original",
+                markersize=3,
+                marker=ORIGINAL_MARKER,
+            )
 
         name = ""
         if (df["pruning"] == 0).all():
@@ -76,85 +98,155 @@ def plot_column_from_dfs(ax: plt.Axes, dfs: List[pd.DataFrame], y: str, x: str =
 
         df.plot(
             x=x,
-            y=f"{y}",
+            y=f"ac_{y}",
             ax=ax,
             c=ALL_COLORS[i],
             marker=ALL_MARKERS[i],
             markersize=3,
             label=name,
         )
+        # original.plot(
+        #     x=x,
+        #     y=f"{y}",
+        #     ax=ax,
+        #     c=ORIGINAL_COLOR,
+        #     linestyle="--",
+        #     label="Original",
+        #     markersize=3,
+        #     marker=ORIGINAL_MARKER,
+        # )
 
 
 def plot_single_dataset(
     dataset: str,
     data_path: str,
-    output_path: str,
+    base_output_path: str,
     format: str = "eps",
     combine: bool = False,
 ):
     assert dataset in ["mnist", "cifar10"], "Dataset must be either mnist or cifar10"
 
     data_path = Path(data_path)
-    output_path = Path(output_path)
-    output_path.mkdir(exist_ok=True, parents=True)
 
-    files = MNIST_FILES if dataset == "mnist" else CIFAR10_FILES
+    original_dataset_pd = pd.read_csv(data_path / f"{dataset}_original.csv")
 
-    dfs = [pd.read_csv(data_path / f) for f in files]
+    for fixed in FIXED_LISTS:
+        files = MNIST_FILES if dataset == "mnist" else CIFAR10_FILES
 
-    n = 1
-    if combine:
-        n = len(COLUMNS)
+        output_path = Path(base_output_path) / fixed
+        output_path.mkdir(exist_ok=True, parents=True)
 
-        fig, axs = plt.subplots(
-            1,
-            n,
-            figsize=(FIG_SIZE[0] * n, FIG_SIZE[1]),
-            constrained_layout=True,
-        )
+        print(f"Output path: {output_path}")
 
-        for i, col in enumerate(COLUMNS):
-            axs[i].set_title(COLUMN_HUMANIZE[i])
-            plot_column_from_dfs(axs[i], dfs, y=col, x="k")
-            axs[i].legend().set_visible(False)
+        dfs = []
 
-        handles, labels = axs[-1].get_legend_handles_labels()
-        # order = [2, 0, 1, 3, 4]
-        order = [2, 0, 1, 3]
-        # order = [2, 0, 1]
-        # print(handles, labels)
-        lgd = fig.legend(
-            [handles[idx] for idx in order],
-            [labels[idx] for idx in order],
-            bbox_to_anchor=(0.5, -0.1),
-            loc="lower center",
-            ncol=5,
-        )
-        # lines_labels = [ax.get_legend_handles_labels() for ax in fig.axes]
-        # lines, labels = [sum(lol, []) for lol in zip(*lines_labels)]
-        # fig.legend(lines, labels)
+        x = fixed.removeprefix("fixed_")
+        x = "k" if x == "sigma" else "sigma"
 
-        # fig.tight_layout()
-        # fig.legend(bbox_to_anchor=(1.25, 0.6), loc="center right")
+        if "sigma" in fixed:
+            dfs = [pd.read_csv(data_path / fixed / f) for f in files]
+        else:
+            dfs = [pd.read_csv(data_path / fixed / f) for f in files if "non" not in f]
 
-        fig.savefig(
-            output_path / f"{dataset}_all.{format}",
-            format=format,
-            dpi=1200,
-            bbox_extra_artists=(lgd,),
-            bbox_inches="tight",
-        )
-        plt.close(fig)
-    else:
-        for col in COLUMNS:
-            fig, ax = plt.subplots(1, 1, figsize=FIG_SIZE, constrained_layout=True)
-            plot_column_from_dfs(ax, dfs, y=col, x="k")
+        print(f"X: {x}")
+
+        # n = 1
+        if combine:
+            fig, axs = plt.subplots(
+                1,
+                2,
+                figsize=(FIG_SIZE[0] * 2, FIG_SIZE[1]),
+                constrained_layout=True,
+            )
+
+            for i, col in enumerate(COLUMNS[:2]):
+                axs[i].set_title(COLUMN_HUMANIZE[i])
+                plot_column_from_dfs(
+                    axs[i],
+                    dfs,
+                    original=original_dataset_pd,
+                    y=col,
+                    x=x,
+                )
+                axs[i].legend().set_visible(False)
+
+            handles, labels = axs[-1].get_legend_handles_labels()
+            # order = [2, 0, 1, 3, 4]
+            # order = [2, 0, 1, 3]
+            # order = [2, 0, 1]
+            # print(handles, labels)
+            lgd = fig.legend(
+                handles,
+                labels,
+                # [handles[idx] for idx in order],
+                # [labels[idx] for idx in order],
+                bbox_to_anchor=(0.5, -0.2),
+                loc="lower center",
+                ncol=3,
+            )
+
             fig.savefig(
-                output_path / f"{dataset}_{col}.{format}",
+                output_path / f"{dataset}_{fixed}_test_acc_time.{format}",
                 format=format,
                 dpi=1200,
+                bbox_extra_artists=(lgd,),
+                bbox_inches="tight",
             )
             plt.close(fig)
+
+            # ------------------------------
+
+            fig, axs = plt.subplots(
+                1,
+                2,
+                figsize=(FIG_SIZE[0] * 2, FIG_SIZE[1]),
+                constrained_layout=True,
+            )
+
+            for i, col in enumerate(COLUMNS[2:]):
+                axs[i].set_title(COLUMN_HUMANIZE[i + 2])
+                plot_column_from_dfs(
+                    axs[i], dfs, original=original_dataset_pd, y=col, x=x, fixed=fixed
+                )
+                axs[i].legend().set_visible(False)
+
+            handles, labels = axs[-1].get_legend_handles_labels()
+            # order = [2, 0, 1, 3, 4]
+            # order = [2, 0, 1, 3]
+            # order = [2, 0, 1]
+            # print(handles, labels)
+            lgd = fig.legend(
+                handles,
+                labels,
+                # [handles[idx] for idx in order],
+                # [labels[idx] for idx in order],
+                bbox_to_anchor=(0.5, -0.2),
+                loc="lower center",
+                ncol=3,
+            )
+
+            fig.savefig(
+                output_path / f"{dataset}_{fixed}_size.{format}",
+                format=format,
+                dpi=1200,
+                bbox_extra_artists=(lgd,),
+                bbox_inches="tight",
+            )
+            plt.close(fig)
+        else:
+            for col in COLUMNS:
+                out_path = output_path / f"{fixed}_{dataset}_{col}.{format}"
+                out_path.parent.mkdir(exist_ok=True, parents=True)
+                fig, ax = plt.subplots(1, 1, figsize=FIG_SIZE, constrained_layout=True)
+                plot_column_from_dfs(
+                    ax, dfs, original=original_dataset_pd, y=col, x=x, fixed=fixed
+                )
+                fig.savefig(
+                    out_path,
+                    format=format,
+                    dpi=1200,
+                )
+                plt.close(fig)
 
 
 def main(
