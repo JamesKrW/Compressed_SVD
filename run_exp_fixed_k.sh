2c65b27896fd7207052e94a7f976e6aea8e67a3d
Ray Andrew <4437323+rayandrew@users.noreply.github.com>
2022-12-01 20:07:09 -0600

    fix typo

--- run_exp_fixed_k.sh
+++ run_exp_fixed_k.sh
@@ -2,7 +2,7 @@
 
 EPOCHS=60
 L2_LAMBDA=0.0
-K=0.6
+k=0.6
 
 ### MNIST
 
@@ -39,7 +39,7 @@ done
 echo "Running CIFAR pruning single layer"
 for sigma in 1 2 3 4 5
 do
-    python train_cifar10.py --k $K --epochs $EPOCHS --l2_lambda $L2_LAMBDA --sigma $sigma --pruning --single_layer
+    python train_cifar10.py --k $k --epochs $EPOCHS --l2_lambda $L2_LAMBDA --sigma $sigma --pruning --single_layer
     sleep 2
 done
 
